4841455bb5dc26111f96ed603515648907f39d13
goh12 <goh12@hi.is>
2018-04-11 23:07:35 +0000

    home bregst við authenticated

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Home extends Component {
-
   render() {
+    const { isAuthenticated } = this.props;
+    console.log(isAuthenticated);
 
     /* todo birta mismunandi upplýsingar ef innskráður notandi eða ekki */
-
-    return (
+    if(isAuthenticated) {
+      return (
       <div className="home">
         <h2>Velkomin á bókasafnið</h2>
-        <p>Til að njóta bókasafnsins til fullnstu mælum við með að <Link to="/login">skrá sig inn</Link>.
-        Þangað til getur þú skoðað <Link to="/books">allar bækrunar</Link>.</p>
+        <p>Þú ert skráður notandi og getur því <Link to="/books">skráð bækur</Link> og breytt <Link to="/profile">þeim sem til eru</Link>.</p>
+        <p>Einnig getur þú skoðað <Link to="/users">aðra notendur</Link>.</p>
       </div>
-    );
+      );
+    } else {
+      return (
+        <div className="home">
+          <h2>Velkomin á bókasafnið</h2>
+          <p>Til að njóta bókasafnsins til fullnstu mælum við með að <Link to="/login">skrá sig inn</Link>.
+          Þangað til getur þú skoðað <Link to="/books">allar bækrunar</Link>.</p>
+        </div>
+      );
+    }
   }
 }
 
 /* todo setja upp tengingu við redux til að vita stöðu notanda */
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+  }
+}
+
+export default connect(mapStateToProps)(Home);
